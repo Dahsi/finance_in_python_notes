8322c5be50393772b4d5270e70d15894d6ee6934
Dahsi <alanchc@gmail.com>
2020-12-10 00:41:02 +0800

    調整為展現常態分配的機率密度函數圖

--- normal_distribution/normal_dist.py
+++ normal_distribution/normal_dist.py
@@ -8,37 +8,73 @@
 import matplotlib.pyplot as plt
 plt.style.use('ggplot')
 
-import numpy as np
 # import pandas as pd
+import numpy as np
+import scipy.stats
 
-def show_normal_dist_histogram():
+# 展現常態分配的機率密度函數圖
+def draw_normal(mu=0, sigma=1, size=10000):
+
     """
     展示三組 normal distribution 圖。
 
     常用參數介紹：
     np.random.normal：
+    https://docs.scipy.org/doc//numpy-1.10.4/reference/generated/numpy.random.normal.html
         loc 期望值
         scale 標準差
         size 基於normal distribution生成的數量
 
     plt.hist：
-        bins: 直方圖的柱數，可選項，默認為10；
-        density: 是否將得到的直方圖各 bin 的數量規一化。默認為 False；
-        color：顏色序列，默認為None；
-        facecolor: 直方圖顏色；
-        edgecolor: 直方圖邊框顏色；
-        alpha: 透明度；
-        histtype: 直方圖類型，『bar』, 『barstacked』, 『step』, 『stepfilled』
+    https://matplotlib.org/3.3.3/api/_as_gen/matplotlib.pyplot.hist.html
+        Parameters:
+            bins: 直方圖的柱數，可選項，默認為10；
+            density: 是否將得到的直方圖各 bin 的數量規一化。默認為 False；
+            color：顏色序列，默認為None；
+            facecolor: 直方圖顏色；
+            edgecolor: 直方圖邊框顏色；
+            alpha: 透明度；
+            histtype: 直方圖類型，『bar』, 『barstacked』, 『step』, 『stepfilled』
+        Returns:
+            n: 每個直方柱對應的數值，若 density=True，則為規一化後的數值
+            bins: 每個分組的邊界數值
+            patches
+
+    scipy.stats.norm.pdf 機率密度函數
+    https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.norm.html
+        Parameters:
+            x: 隨機變量數值
+            loc: 期望值
+            scale: 標準差
+        Returns:
+            y: 機率密度函數數值
 
     :return: None
     """
-    plt.hist(np.random.normal(loc=-2, scale=0.5, size=10000), bins=50, density=True, color='g', alpha=0.5)
-    plt.hist(np.random.normal(loc=0, scale=1, size=10000), bins=50, density=True, color='b', alpha=0.5)
-    plt.hist(np.random.normal(loc=2, scale=1.5, size=10000), bins=50, density=True, color='r', alpha=0.5)
+    np.random.seed(0)
+    dnormal = np.random.normal(mu, sigma, size)
+    _, bins_edge, _ = plt.hist(dnormal, bins=50, density=True, alpha=0.5)
+
+    # 機率密度函數曲線
+    y = scipy.stats.norm.pdf(bins_edge, mu, sigma)
+    plt.plot(bins_edge, y, label='$\mu$=%.1f, $\sigma^2$=%.1f'%(mu, sigma))
+
+    plt.xlabel('Expectation')
+    plt.ylabel('Probability')
+    plt.title('histogram of normal distribution:')
+
+    return True
+
+
+def show_normal_dist_plot():
+    draw_normal(mu=0, sigma=1, size=10000)
+    draw_normal(mu=-2, sigma=0.5, size=10000)
+    draw_normal(mu=2, sigma=1.5, size=10000)
+    plt.legend(loc=0, ncol=1)
     plt.show()
 
 
 # --------------------------------------------------------------------------------
 if __name__ == '__main__':
-    show_normal_dist_histogram()
+    show_normal_dist_plot()
     pass
